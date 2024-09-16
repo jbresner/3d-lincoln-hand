58b4d444efaf5bafd6b8566bd3934bfbb0ebe292
Jamie Bresner <jbresner@nearestexit.com>
2024-09-15 17:08:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function init() {
 
     // Load the STL file
     const loader = new THREE.STLLoader();
-    loader.load('./path/to/your-large-file.stl', function (geometry) {
+    loader.load('./hand.stl', function (geometry) {
         geometry.center();  // Center the geometry
 
         // Use MeshStandardMaterial (basic PBR material) for default lighting
